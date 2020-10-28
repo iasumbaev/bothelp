17f605ce0c8f7c4f7532a9f065671d396e8f019b
iasumbaev@gmail.com <gPg5g3zd8dZULe8>
2020-10-29 02:23:26 +0700

    Fix

--- index.php
+++ index.php
@@ -14,13 +14,13 @@ $client = new Client([
     'port' => 6379,
 ]);
 
-for ($i = 0; $i < 256 && $this->client->llen('events') !== 0; $i++) {
+for ($i = 0; $i < 256 && $client->llen('events') !== 0; $i++) {
     echo 'Action: ' . $i . PHP_EOL;
     action();
 }
 
 // Ожидание, пока обработчики не закончат работу
-while ($this->client->llen('events') !== 0)
+while ($client->llen('events') !== 0)
 {
     continue;
 }
