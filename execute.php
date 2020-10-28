ce71326c5fac02bcc8aafe2a82c8f3c785caabe9
iasumbaev@gmail.com <gPg5g3zd8dZULe8>
2020-10-28 17:08:02 +0700

    Add echo

--- execute.php
+++ execute.php
@@ -11,6 +11,7 @@ $channel = $connection->channel();
 $channel->queue_declare('event_queue', false, true, false, false);
 
 for ($i = 0; $i < 10; $i++) {
-   $handler =  new EventHandler($channel);
-   $handler->execute();
+    echo 'Start handler: ' . $i;
+    $handler = new EventHandler($channel);
+    $handler->execute();
 }
