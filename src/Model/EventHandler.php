61562f193d45259546249ad784f681242137a2fb
iasumbaev@gmail.com <gPg5g3zd8dZULe8>
2020-10-29 13:21:01 +0700

    Check pool

--- src/Model/EventHandler.php
+++ src/Model/EventHandler.php
@@ -46,6 +46,10 @@ class EventHandler
         $pool = array_map('intval', $pool);
         sort($pool);
 
+        if (!isset($pool[0])) {
+            //TODO: think
+            exit;
+        }
         // Если событие первое в пуле, то его можно выполнить
         return $pool[0] === (int)$eventID;
 
