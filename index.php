3dc4323ee58c5cc73f30a6116103e53f3960ff94
iasumbaev@gmail.com <gPg5g3zd8dZULe8>
2020-10-30 03:36:56 +0700

    Test

--- index.php
+++ index.php
@@ -28,12 +28,12 @@ file_put_contents('log.txt', '');
 
 $start = microtime(true);
 $command = 'php execute.php';
-for ($i = 0; $i < 10 && $client->llen('events'); $i++) {
+//for ($i = 0; $i < 10 && $client->llen('events'); $i++) {
 //while ($client->llen('events') !== 0) {
-//    if (getProcessCount($command) < 50) {
+if (getProcessCount($command) < 50) {
     execInBackground($command);
-//    }
 }
+//}
 
 echo 'Length now: ' . $client->llen('events') . PHP_EOL;
 $loopCount = 0;
